3426bf3ead23c21eb8d2e4c5043597705424116f
Jack Söderblom <dunxiii@gmail.com>
2016-08-27 17:38:50 +0200

    Better installation

--- install.sh
+++ install.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 shopt -s nullglob
 
+HOOKS_DEST="/etc/pacman.d/hooks"
 SH_DEST="/usr/local/bin"
 SERVICE_DEST="/etc/systemd/system"
+PWD="$(cd $(dirname $0) && pwd)"
 
-for file in ./{i3,arch,sysadmin}/*.sh; do
+if [[ ! -d "${HOOKS_DEST}" ]]; then
+    mkdir -p "${HOOKS_DEST}"
+fi
+
+for file in ${PWD}/arch/pacman/hooks/*.hook; do
+    ln -fs "$(realpath "${file}")" "${HOOKS_DEST}/"
+done
+
+for file in ${PWD}/{i3,sysadmin}/*.sh; do
     ln -fs "$(realpath "${file}")" "${SH_DEST}/"
 done
 
 # Host specific
-for file in ./$(hostname)/*.sh; do
+for file in ${PWD}/$(hostname)/*.sh; do
     ln -fs "$(realpath "${file}")" "${SH_DEST}/"
 done
 
-for file in ./$(hostname)/service/*.service; do
+for file in ${PWD}/$(hostname)/service/*.service; do
     ln -fs "$(realpath "${file}")" "${SERVICE_DEST}/"
     # Systemd bug forces me to use realpath instead of basename...
     # https://github.com/systemd/systemd/issues/3010
