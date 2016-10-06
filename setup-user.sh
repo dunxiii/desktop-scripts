403d305c9c51a4e5e12756113a56952a4695f227
Jack Söderblom <dunxiii@gmail.com>
2016-10-06 23:04:55 +0200

    Script to setup user for first time

---
+++ setup-user.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Create some default directories
+mkdir ~/Bin
+mkdir ~/Documents
+mkdir ~/Downloads
+mkdir ~/Insync
+mkdir ~/Pictures
+mkdir ~/Public
+mkdir -p ~/.vim/undodir
+
+# Fetch dotfiles from github
+git clone https://github.com/dunxiii/dotfiles.git ~/Git/dotfiles
+
+# Switch git repo from https to ssh
+cd ~/Git/dotfiles && git remote set-url origin git@github.com:dunxiii/dotfiles.git
+
+# Deploy dotfiles
+~/Git/dotfiles/install
+
+# Install oh my zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+# Install vim-plug
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
