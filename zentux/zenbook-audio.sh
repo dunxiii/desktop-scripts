9dd8f937dae037a044c8279608f7148c4d45ab8c
Jack Söderblom <dunxiii@gmail.com>
2017-01-07 18:29:52 +0100

    possibly better way to get current sink

--- zentux/zenbook-audio.sh
+++ zentux/zenbook-audio.sh
@@ -3,7 +3,13 @@
 STEP=2
 
 get_active_sink() {
-    ACTIVE_SINK=$(pacmd list-sinks | grep '* index:' | grep -o '[0-9]*')
+    # If one sink is running
+    ACTIVE_SINK=$(pacmd list-sinks | grep -B 4 RUNNING | head -n 1 | grep -o '[0-9]*')
+
+    # Else take default
+    if [[ ${ACTIVE_SINK} == "" ]]; then
+        ACTIVE_SINK=$(pacmd list-sinks | grep '* index:' | grep -o '[0-9]*')
+    fi
 }
 
 set_volume() {
@@ -26,14 +32,21 @@ mute_sink() {
     pactl set-sink-mute ${ACTIVE_SINK} toggle
 }
 
-while getopts ":v:s:fmt" option; do
+while getopts ":v:s:fmto" option; do
     case ${option} in
         v) set_volume ${OPTARG} ;;
         f) follow_sources_to_active_sink ;;
         m) mute_sink ;;
         t) toggle_sink ;;
         s) STEP=${OPTARG} ;;
+        o) output_vol=true
     esac
 done
+
+if [[ ${output_vol} ]]; then
+    get_active_sink
+    pactl list sinks | grep '^[[:space:]]Volume:' | \
+        head -n $(( ${ACTIVE_SINK} + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
+fi
 
 exit 0
